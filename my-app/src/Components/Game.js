ebcd0cc3ef4715d110f9720f56230af8517f8d01
Marc Räder <marc.raeder@5minds.de>
2023-01-16 14:26:14 +0100

    fix bugs

--- my-app/src/Components/Game.js
+++ my-app/src/Components/Game.js
@@ -1,60 +1,55 @@
 import React, { useEffect, useRef, useState } from "react";
-import { GenerateNewText } from "./GenerateNewText";
 
 export function Game(props) {
-  const [current, setCurrent] = useState(props.text[0]);
-  const [text, setText] = useState(props.text);
-  const [typedChars, setTypedChars] = useState([]);
   const divRef = useRef(HTMLDivElement);
   const [wrongChar, setWrongChar] = useState(false);
+  const current = props.text[0];
 
   useEffect(() => {
-    setText(props.text.slice(1));
-    setCurrent(props.text[0]);
+    props.setText(props.text.slice(1));
     divRef.current.focus();
-  }, [props.text]);
+  }, []);
 
   const onKeyPressed = (event) => {
     const correct = event.key === current && wrongChar === false;
     const incorrect = event.key !== current;
     const firstIncorrectThenIncorrect = current && wrongChar === true;
+
     if (correct) {
-      const copyTypedChars = typedChars.slice();
+      const copyTypedChars = props.typedChars.slice();
       copyTypedChars.push({
         char: current,
         status: "correct",
       });
-      setTypedChars(copyTypedChars);
-      setCurrent(text[0]);
-      setText(text.slice(1));
+      props.setTypedChars(copyTypedChars);
+      props.setText(props.text.slice(1));
     } else if (incorrect) {
       setWrongChar(true);
     } else if (firstIncorrectThenIncorrect) {
-      const copyTypedChars = typedChars.slice();
+      const copyTypedChars = props.typedChars.slice();
       copyTypedChars.push({
         char: current,
         status: "incorrect",
       });
-      setTypedChars(copyTypedChars);
-      setCurrent(text[0]);
-      setText(text.slice(1));
+      props.setTypedChars(copyTypedChars);
+      props.setText(props.text.slice(1));
       setWrongChar(false);
     }
   };
 
   return (
-    <>
+    <div className="TypingArea__Text">
       <div ref={divRef} onKeyDown={onKeyPressed} tabIndex="0">
         <>
-          {typedChars.map((element, i) => (
+          {props.typedChars.map((element, i) => (
             <span key={i} className={`TypingArea__Text--${element.status === "correct" ? "" : "in"}correct`}>
               {element.char}
             </span>
           ))}
         </>
         <span className="TypingArea__Text--current">{current}</span>
-        {text}
+        {props.text.slice(1)}
       </div>
-    </>
+    </div>
   );
 }
--- my-app/src/Components/TypingArea.js
+++ my-app/src/Components/TypingArea.js
@@ -1,26 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "./Button";
+import { Game } from "./Game";
 import { GenerateNewText } from "./GenerateNewText";
 import { TextLengthInput } from "./TextLengtInput";
-import { TypingField } from "./TypingField";
 
 export function TypingArea() {
   const [text, setText] = useState("");
   const [textLength, setTextLength] = useState(5);
+  const [typedChars, setTypedChars] = useState([]);
+  const [backupText, setBackupText] = useState("");
   useEffect(() => {
-    GenerateNewText(textLength).then((response) => setText(response));
+    GenerateNewText(textLength).then((response) => {
+      setText(response);
+      setBackupText(response);
+    });
   }, []);
+
+  function newRun() {
+    GenerateNewText(textLength).then((response) => {
+      setText(response);
+      setBackupText(response);
+      setTypedChars([]);
+    });
+  }
+
+  if (text.length === 0 && typedChars.length !== 0) {
+    newRun();
+  }
 
   return (
     <div className="TypingArea">
       <h1 className="TypingArea__Header ">Typing 100</h1>
       <TextLengthInput setTextLength={setTextLength} />
-      <TypingField text={text} />
+      <Game text={text} setText={setText} typedChars={typedChars} setTypedChars={setTypedChars} />
       <div className="TypingArea__ButtonLine">
-        <Button text="Reset Run" />
+        <Button
+          text="Reset Run"
+          onClick={() => {
+            setText(backupText);
+            setTypedChars([]);
+          }}
+        />
         <Button
           text="Generate New Text"
-          onClick={() => GenerateNewText(textLength).then((response) => setText(response))}
+          onClick={() => {
+            newRun();
+          }}
         />
       </div>
     </div>
