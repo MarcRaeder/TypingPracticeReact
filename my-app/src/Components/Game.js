8c0a6367e0cb5e100649d8e71f0e2cbb49667b76
Marc Räder <110394079+MarcRaeder@users.noreply.github.com>
2023-01-24 10:34:39 +0100

    Revert "add BasicStructure"

--- my-app/src/Components/Game.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Game(props) {
-  return <div className="TypingArea__Text">{props.text}</div>;
-}
--- my-app/src/Components/Stats.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Stats(props) {
-  return <div className="StatsArea__Stats">{props.text}</div>;
-}
