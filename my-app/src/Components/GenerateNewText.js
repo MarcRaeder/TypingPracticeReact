a2369b727422a3fc0721684ac7470d1e4adee279
Marc Räder <marc.raeder@5minds.de>
2023-02-25 17:24:37 +0100

    delete file

--- my-app/src/Components/GenerateNewText.js
+++
@@ -1,9 +0,0 @@
-export async function GenerateNewText(textLength) {
-  let file = "https://random-word-api.herokuapp.com/word?number=" + textLength;
-  Math.floor(Math.random() * 100);
-  let response = await fetch(file);
-  let words = await response.json();
-  let text = words.join(" ");
-
-  return text;
-}
